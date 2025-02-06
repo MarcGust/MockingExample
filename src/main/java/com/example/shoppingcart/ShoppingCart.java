641b78f307085afaff461741ea0db2e5c4d6ed79
MarcGust <marcus.gustavsson@iths.se>
2025-02-06 21:29:05 +0100

    add Item class and refactor

--- src/main/java/com/example/shoppingcart/ShoppingCart.java
+++ src/main/java/com/example/shoppingcart/ShoppingCart.java
@@ -48,28 +48,4 @@ public class ShoppingCart {
     public int getItemQuantity(String itemName) {
         return items.containsKey(itemName) ? items.get(itemName).getQuantity() : 0;
     }
-
-    private static class Item {
-        private String name;
-        private double price;
-        private int quantity;
-
-        public Item(String name, double price, int quantity) {
-            this.name = name;
-            this.price = price;
-            this.quantity = quantity;
-        }
-
-        public double getPrice() {
-            return price;
-        }
-
-        public int getQuantity() {
-            return quantity;
-        }
-
-        public void setQuantity(int quantity) {
-            this.quantity = quantity;
-        }
-    }
 }
